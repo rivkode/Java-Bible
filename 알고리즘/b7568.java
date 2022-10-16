86bac977bdecefb6577ab3ad708622a40d83bb56
jonghun <jonghuncu@gmail.com>
2022-10-17 03:01:53 +0900

    1017

---
+++ 알고리즘/b7568.java
@@ -0,0 +1,31 @@
+package backjoon;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class b7568 {
+    public static void main(String[] args) {
+        ArrayList<Integer[]> lst = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=0; i<n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            lst.add(new Integer[]{a, b});
+        }
+
+        for( int i=0; i<n; i++) {
+            int rank = 1;
+
+            for(int j=0; j<n; j++) {
+                if(i==j) continue;
+                if(lst.get(i)[0] < lst.get(j)[0] && lst.get(i)[1] < lst.get(j)[1]) {
+                    rank++;
+                }
+            }
+            System.out.printf(rank+ " ");
+        }
+    }
+}
