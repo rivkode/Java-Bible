8fb6b83de37b20fa39adfeb78ad59ea0397dd477
jonghun <jonghuncu@gmail.com>
2022-11-01 11:47:50 +0900

    Chore : 반복문

---
+++ 알고리즘/c1071.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class c1071 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        while(true) {
+            int tmp = in.nextInt();
+            if(tmp == 0) {
+                break;
+            }
+            System.out.println(tmp);
+        }
+    }
+}
