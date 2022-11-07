a8869caf4c444c3c3b0e0916e23cb4eeb1823765
jonghun <jonghuncu@gmail.com>
2022-11-07 13:07:50 +0900

    Docs : update 자바의 정석 Week2

---
+++ src/JavaBible/javaBible_week2/composite.java
@@ -0,0 +1,17 @@
+package JavaBible.javaBible_week2;
+
+class Point {
+    int x;
+    int y;
+}
+
+class Circle {
+    Point p = new Point(); // 포함관계 Circle 안에 멤버로 point라는 참조변수를 선언함
+    int r;
+}
+
+public class composite {
+    public static void main(String[] args) {
+        Circle c = new Circle();
+    }
+}
