a2d7c055036c6a1527e8b6fbd2b428cef84cbd7f
jonghun <jonghuncu@gmail.com>
2022-11-07 13:04:33 +0900

    Chore : update Stack 예제코드 및 md 파일

---
+++ src/Algorithm/Stack/c1402.java
@@ -0,0 +1,22 @@
+package Stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class c1402 {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+
+        for(int i=0; i<num; i++) {
+            stack.push(in.nextInt());
+        }
+
+        for(int i=0; i<num; i++) {
+            int peek = stack.peek();
+            System.out.println(peek);
+            stack.pop();
+        }
+    }
+}
