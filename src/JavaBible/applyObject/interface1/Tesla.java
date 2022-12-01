e885502ba9e0dbe2517fc4c16c6f38b47ccc0f6f
jonghun <jonghuncu@gmail.com>
2022-12-01 23:59:37 +0900

    chore(java) : 인터페이스

---
+++ src/JavaBible/applyObject/interface1/Tesla.java
@@ -0,0 +1,12 @@
+package JavaBible.applyObject.interface1;
+
+public class Tesla {
+    public static final int wheel = 4;
+    public String name;
+
+    public static void main(String[] args) {
+        Tesla tesla = new Tesla();
+        tesla.name = "Model3";
+        System.out.println(tesla.name);
+    }
+}
