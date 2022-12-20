2c386e7c5487da6a8967903d80e28fbe06632f53
jonghun <jonghuncu@gmail.com>
2022-12-20 18:23:06 +0900

    learn(java) : SRP

---
+++ src/JavaBible/applyObject/SRP/Phone.java
@@ -0,0 +1,35 @@
+package JavaBible.applyObject.SRP;
+
+// SRP : single responsibility principle 단일 책임 원칙
+
+public class Phone {
+    void call() {
+
+    }
+    void getCharge() {
+
+    }
+}
+
+class user {
+    void clean() {
+
+    }
+
+    void checkCharge() {
+
+    }
+
+}
+
+class developer{
+    void error() {
+
+    }
+    void AppDevelop() {
+
+    }
+
+}
+
+// 결론 : 변경으로 버그가 발생하더라도 다른 관련 없는 동작에 영향을 미치지 않도록 동작 분리를 목표로 함.
