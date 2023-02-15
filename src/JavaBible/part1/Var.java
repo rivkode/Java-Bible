4f9b88696518349c6c5dece5cbc0688ef7831e8b
jonghun <jonghuncu@gmail.com>
2023-02-15 22:30:07 +0900

    learn : 자바의 정석

---
+++ src/JavaBible/part1/Var.java
@@ -0,0 +1,13 @@
+package JavaBible.part1;
+
+public class Var {
+    public static void main(String[] args) {
+        int score = 200;
+        score = 100;
+
+        System.out.println("score = " + score);
+        final int car = 1;
+        System.out.println("car = " + car);
+//        car = 2;
+    }
+}
